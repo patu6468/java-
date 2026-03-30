c0e61b448ac54af2ceae6c1ec15ccb79383ddb60
patu6468 <pratikshirgire2004@gmail.com>
2026-03-30 18:40:20 +0530

    Update main.java

--- main.java
+++ main.java
@@ -4,5 +4,14 @@ public class main {
         public static void main(String[] args) {
             System.out.println("hello java");
             System.out.println("hello git");
+
+                System.out.println("hello java");
+            System.out.println("hello git");
+
+                System.out.println("hello java");
+            System.out.println("hello git");
+
+                System.out.println("hello java");
+            System.out.println("hello git");
         }  
 }
